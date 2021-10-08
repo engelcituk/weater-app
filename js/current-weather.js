af0be7ac6a22194fd093ed73f0c1a800fd8c9ea8
cituk <programaciondyn3.mx@sandos.com>
2021-10-07 23:49:59 -0500

    Creando módulo del clima actual

---
+++ js/current-weather.js
@@ -0,0 +1,16 @@
+import weather from './../data/current-weather.js'
+
+function configCurrentWeather( weather ) {
+    //loader
+
+    //date
+
+    //city
+
+    // temp
+
+    //backgroudn
+}
+export default function currentWeater() {
+    configCurrentWeather( weather )
+}
