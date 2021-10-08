a040704ddbe2f692b4627afaba8b24de990501f1
cituk <programaciondyn3.mx@sandos.com>
2021-10-08 00:05:25 -0500

    Renderizando la ciudad origen, DOM Elements

--- js/current-weather.js
+++ js/current-weather.js
@@ -1,12 +1,18 @@
 import weather from './../data/current-weather.js'
 
+function setCurrentCity(element, city) {
+    element.textContent = city
+    
+}
 function configCurrentWeather( weather ) {
     //loader
 
     //date
 
     //city
-
+    const currentWeatherCity = document.querySelector('#current-weather-city')
+    const city = weather.name
+    setCurrentCity( currentWeatherCity,  city) 
     // temp
 
     //backgroudn
